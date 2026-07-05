9709f02421edb1f54ab6f919b17892e9f5e7846a
Johan Silkens <me@jsilkens.be>
2026-07-05 20:41:23 +0200

    feat: add Maven wrapper properties and placeholder scripts

---
+++ mvnw.cmd
@@ -0,0 +1,6 @@
+@REM Maven Wrapper placeholder for Windows
+@REM After cloning, generate the full wrapper with:
+@REM   mvn wrapper:wrapper -Dmaven=3.9.14
+
+@echo Maven Wrapper not installed. Run: mvn wrapper:wrapper -Dmaven=3.9.14
+@exit /b 1
